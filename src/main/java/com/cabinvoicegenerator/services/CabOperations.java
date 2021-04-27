5956a29dda6da0a1cb6855fcbebaaa9db57c76e4
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-04-27 20:56:41 +0530

    ADDED : ability to calculate total fare for the ride

---
+++ src/main/java/com/cabinvoicegenerator/services/CabOperations.java
@@ -0,0 +1,11 @@
+package com.cabinvoicegenerator.services;
+
+public class CabOperations {
+    private static final double FARE_PER_KM = 10;
+    private static final double FARE_PER_MINUTE = 1;
+
+    public double calculateCostOfRide(double totalRidingDistance, double totalRidingTimeInHours){
+        double totalRidingTimeInMin = totalRidingTimeInHours * 60;
+        return totalRidingDistance * FARE_PER_KM + totalRidingTimeInMin * FARE_PER_MINUTE;
+    }
+}
---
+++ src/test/java/com/cabinvoicegenerator/services/CabOperationsTest.java
@@ -0,0 +1,19 @@
+package com.cabinvoicegenerator.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CabOperationsTest {
+    CabOperations cabOperations = new CabOperations();
+
+    @Test
+    public void givenRidingDetails_WhenGiven_ShouldReturnFare(){
+        double taxiDistance = 25.0;
+        double taxiTime = 3.0;
+        double totalFare = cabOperations.calculateCostOfRide(taxiDistance, taxiTime);
+        Assertions.assertEquals(430, totalFare);
+    }
+
+}
