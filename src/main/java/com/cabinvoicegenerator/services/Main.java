d024f64973f5fc8e5c14ff1734abcd63944ce451
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-04-27 20:46:21 +0530

    STARTED : added welcome message to the program

---
+++ src/main/java/com/cabinvoicegenerator/services/Main.java
@@ -0,0 +1,7 @@
+package com.cabinvoicegenerator.services;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO THE CAB INVOICE GENERATOR PROGRAM");
+    }
+}
