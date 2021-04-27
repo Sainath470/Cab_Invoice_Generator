6ccc5d37f67bb1e40d2327ff955f276781aeec36
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-04-27 21:03:00 +0530

    ADDED : minimum fare would be rs 5

--- src/main/java/com/cabinvoicegenerator/services/CabOperations.java
+++ src/main/java/com/cabinvoicegenerator/services/CabOperations.java
@@ -3,9 +3,12 @@ package com.cabinvoicegenerator.services;
 public class CabOperations {
     private static final double FARE_PER_KM = 10;
     private static final double FARE_PER_MINUTE = 1;
+    private static final double MINIMUM_FARE = 5;
 
     public double calculateCostOfRide(double totalRidingDistance, double totalRidingTimeInHours){
         double totalRidingTimeInMin = totalRidingTimeInHours * 60;
-        return totalRidingDistance * FARE_PER_KM + totalRidingTimeInMin * FARE_PER_MINUTE;
+      double totalFare = totalRidingDistance * FARE_PER_KM + totalRidingTimeInMin * FARE_PER_MINUTE;
+
+        return Math.max(totalFare, MINIMUM_FARE);
     }
 }
